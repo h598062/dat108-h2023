1398905cff60941f685a11f532eea916b39c2918
Bjørnar <598062@stud.hvl.no>
2023-09-01 14:02:15 +0200

    fra timen

--- f04-eksempler-skeleton/src/no/hvl/dat108/f04/Eksempel4.java
+++ f04-eksempler-skeleton/src/no/hvl/dat108/f04/Eksempel4.java
@@ -1,27 +1,32 @@
 package no.hvl.dat108.f04;
+
 import static no.hvl.dat108.f04.People.people;
 
 /*
  * Eksempel4
- * 
+ *
  * Vårt første stream()-eksempel.
  * Skrive ut alle med fornavn som begynner på "C".
  */
 public class Eksempel4 {
 
 	public static void main(String[] args) {
-		
-		//Skrive ut alle med fornavn som begynner på "C"
 
-		//Gamlemåten:		
-//		for (Person p : people) {
-//			if (p.getFirstName().startsWith("C")) {
-//				System.out.println(p);
-//			}
-//		}
-		
-		//Med streams:
-		//...
+		// Skrive ut alle med fornavn som begynner på "C"
+
+		// Gamlemåten:
+		//		for (Person p : people) {
+		//			if (p.getFirstName().startsWith("C")) {
+		//				System.out.println(p);
+		//			}
+		//		}
+
+		// Med streams:
+		people.stream()
+		      .filter(p -> p.getFirstName()
+		                    .startsWith("C"))
+		      .forEach(System.out::println);
+
 	}
 }
 
