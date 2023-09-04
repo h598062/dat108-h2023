34fadb35c59bf728a190be5a36fd6c823d2ae560
Bjørnar <598062@stud.hvl.no>
2023-09-04 17:00:46 +0200

    endringer fra time

--- f05-eksempler-skeleton/src/no/hvl/dat108/f04/fikse/Eksempel7.java
+++ f05-eksempler-skeleton/src/no/hvl/dat108/f04/fikse/Eksempel7.java
@@ -2,31 +2,44 @@ package no.hvl.dat108.f04.fikse;
 
 /*
  * Eksempel7 / Gjøre sammen i timen
- * 
- * Øve oss litt på litt flere grunnleggende 
+ *
+ * Øve oss litt på litt flere grunnleggende
  *  - source		IntStream.range(), Stream.iterate(), Stream.of()
  *  - intermediate	filter(), map(), limit(), distinct(), sorted()
  *  - terminal 		forEach(), collect(), toList()
- *  
+ *
  * operations på streams.
  */
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Eksempel7 {
-	
+
 	public static void main(String[] args) {
-		
+
 		// ... Har fjernet de tingene vi gjorde sist gang ...
-		
-		//For en stream av strenger, samle dem opp i en ny string, sortert
-		//og uten duplikater. Skriv deretter ut resultatet.
+
+		// For en stream av strenger, samle dem opp i en ny string, sortert
+		// og uten duplikater. Skriv deretter ut resultatet.
 		System.out.println("\n");
 		System.out.println("Sorterte strenger uten duplikater");
-		//TODO
-		
-		//For en stream av strenger, samle dem sortert på stringlengde, 
-		//uten duplikater i en ny liste. Skriv deretter ut listen.
+		String s = Stream.of("Per", "Knut", "Anne", "Per")
+		                 .distinct()
+		                 .sorted()
+		                 .collect(Collectors.joining(" "));
+		System.out.println(s);
+		// For en stream av strenger, samle dem sortert på stringlengde,
+		// uten duplikater i en ny liste. Skriv deretter ut listen.
 		System.out.println();
 		System.out.println("Sorterte strenger uten duplikater");
-		//TODO
+		List<String> ls = Stream.of("Per", "Knuto", "Anne", "Per")
+		                        .distinct()
+		                        //.sorted((a, b) -> a.length() - b.length())
+		                        .sorted(Comparator.comparingInt(String::length))
+		                        .toList();
+		System.out.println(ls);
 	}
 }
--- f05-eksempler-skeleton/src/no/hvl/dat108/f05/Eks1reduce.java
+++ f05-eksempler-skeleton/src/no/hvl/dat108/f05/Eks1reduce.java
@@ -3,40 +3,57 @@ package no.hvl.dat108.f05;
 import static no.hvl.dat108.f05.People.people;
 
 public class Eks1reduce {
-	
+
 	public static void main(String[] args) {
-		
+
 		/* Summen av aldre av personer i people-listen */
-		
+
 		// Summen av aldre med vanlig +
-//		int sumAlder = TODO
-		
+		int sumAlder1 = people.stream()
+		                      .map(Person::getAge)
+		                      .reduce(0, (sum, alder) -> sum + alder);
+
 		// Alternativt med Integer.sum
-//		int sumAlder = TODO
-		
+		int sumAlder2 = people.stream()
+		                      .map(Person::getAge)
+		                      .reduce(0, (akk, alder) -> Integer.sum(akk, alder));
+
 		// Med metodereferanse
-//		int sumAlder = TODO
-		
+		int sumAlder3 = people.stream()
+		                      .map(Person::getAge)
+		                      .reduce(0, Integer::sum);
+
 		// Alternativt med IntStream (Stream av int)
-//		int sumAlder = TODO
-		
-//		System.out.println(sumAlder);
-		
-		
+		int sumAlder4 = people.stream()
+		                      .mapToInt(Person::getAge)
+		                      .sum();
+
+		System.out.println(sumAlder1 + " " + sumAlder2 + " " + sumAlder3 + " " + sumAlder4);
+
+
 		/* En streng med alle initialene, "CD LC TC CB MA" */
 
 		// Samle opp med reduce og +
-//		String initialer = TODO
+		String initialer1 = people.stream()
+		                          .map(p -> "" + p.getFirstName()
+		                                          .charAt(0))
+		                          .reduce("", (res, c) -> res + " " + c);
 
 		// Fjerne ende-spaces med trim 
-//		String initialer = TODO
+		String initialer2 = people.stream()
+		                          .map(p -> "" + p.getFirstName()
+		                                          .charAt(0))
+		                          .reduce("", (res, c) -> res + " " + c)
+		                          .trim();
+
 
 		// Alternativt med collect og Collectors.joining
-//		String initialer = TODO
+		String initialer3 = "";
 
-//		System.out.println("|" + initialer + "|");
-		
-	}	
+		System.out.println("|" + initialer1 + "|");
+		System.out.println("|" + initialer2 + "|");
+		System.out.println("|" + initialer3 + "|");
+	}
 
 }
 
