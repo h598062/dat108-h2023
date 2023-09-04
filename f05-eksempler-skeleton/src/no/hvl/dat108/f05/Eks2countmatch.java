14bb82f3b3e2e582eef819d1572feb2341a8ec7d
Bjørnar <598062@stud.hvl.no>
2023-09-04 17:00:46 +0200

    endringer fra time

--- f05-eksempler-skeleton/src/no/hvl/dat108/f05/Eks2countmatch.java
+++ f05-eksempler-skeleton/src/no/hvl/dat108/f05/Eks2countmatch.java
@@ -11,20 +11,20 @@ public class Eks2countmatch {
 
 		//Antall personer over 50 år - count()
 		long antallPersoner = people.stream()
-				.filter(p -> p.getAge() > 50)
-				.count();
+		                            .filter(p -> p.getAge() > 50)
+		                            .count();
 		System.out.println(antallPersoner);
 		
 		//Om vi har data som matcher - anyMatch, allMatch, noneMatch
 		
 		//Er alle over 30 år?
 		boolean alleOver30 = people.stream()
-				.allMatch(p -> p.getAge() > 30);
+		                           .allMatch(p -> p.getAge() > 30);
 		System.out.println("Alle over 30? " + alleOver30);
 		
 		//Er noen over 60 år?
 		boolean noenOver60 = people.stream()
-				.anyMatch(p -> p.getAge() > 60);
+		                           .anyMatch(p -> p.getAge() > 60);
 		System.out.println("Noen over 60? " + noenOver60);
 
 	}
