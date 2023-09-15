6e24db37c8da4d267672d26fd45c582f01643a06
Bjørnar <598062@stud.hvl.no>
2023-09-15 13:50:16 +0200

    fra timer

--- f08-eksempler-skeleton/src/lockawaitsignal/Melding.java
+++ f08-eksempler-skeleton/src/lockawaitsignal/Melding.java
@@ -1,15 +1,37 @@
 package lockawaitsignal;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Melding {
 
 	private String tekst;
 
+	private Lock      lock     = new ReentrantLock();
+	private Condition harTekst = lock.newCondition();
+
 	public String getTekst() {
+		lock.lock();
+		try {
+			while (!harTekst()) {
+				harTekst.await();
+			}
+		} catch (InterruptedException e) {
+		} finally {
+			lock.unlock();
+		}
 		return tekst;
 	}
 
 	public void setTekst(String tekst) {
-		this.tekst = tekst;
+		lock.lock();
+		try {
+			this.tekst = tekst;
+			harTekst.signalAll();
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	public boolean harTekst() {
