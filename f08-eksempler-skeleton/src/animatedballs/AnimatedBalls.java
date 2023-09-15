e1e2d4f7b386dc7b6eab815d720c0f094621d9ff
Bjørnar <598062@stud.hvl.no>
2023-09-15 13:50:16 +0200

    fra timer

--- f08-eksempler-skeleton/src/animatedballs/AnimatedBalls.java
+++ f08-eksempler-skeleton/src/animatedballs/AnimatedBalls.java
@@ -11,30 +11,34 @@ import javax.swing.JPanel;
 public class AnimatedBalls {
 
 	public static void main(String[] args) {
-		
-		//Her oppretter vi et ball-objekt med gitt farge
-		Ball roedBall = new Ball(Color.RED);
-		
-		//Her oppretter vi en tegneflate og legger til ballobjektet
+
+		// Her oppretter vi et ball-objekt med gitt farge
+		Ball roedBall  = new Ball(Color.RED);
+		Ball blaaBall  = new Ball(Color.BLUE);
+		Ball gronnBall = new Ball(Color.GREEN);
+
+		// Her oppretter vi en tegneflate og legger til ballobjektet
 		JPanel tegneflate = new JPanel();
 		tegneflate.setLayout(null);
 		tegneflate.add(roedBall);
-		
-		//Her oppretter vi selve 2D-grafikk-vinduet legger til tegneflaten
+		tegneflate.add(blaaBall);
+		tegneflate.add(gronnBall);
+
+		// Her oppretter vi selve 2D-grafikk-vinduet legger til tegneflaten
 		JFrame grafikkvinduet = new JFrame();
 		grafikkvinduet.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		grafikkvinduet.setLayout(new BorderLayout());
 		grafikkvinduet.setSize(PANE_SIZE, PANE_SIZE);
 		grafikkvinduet.add(tegneflate);
-		
-		//Her starter vi applikasjonen ved å gjøre grafikkvinduet synlig
-		//og be ballen om å gjøre animasjonen, dvs. bevege og tegne seg 
-		//på nytt i evig løkke.
-		grafikkvinduet.setVisible(true);
-		
-		roedBall.animate();
-	}
 
+		// Her starter vi applikasjonen ved å gjøre grafikkvinduet synlig
+		// og be ballen om å gjøre animasjonen, dvs. bevege og tegne seg
+		// på nytt i evig løkke.
+		grafikkvinduet.setVisible(true);
+		new Thread(roedBall::animate).start();
+		new Thread(blaaBall::animate).start();
+		new Thread(gronnBall::animate).start();
+	}
 }
 
 
