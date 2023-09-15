f862ad99e4556a46108b7e7e20f6f590465d44e0
Bjørnar <598062@stud.hvl.no>
2023-09-15 13:50:16 +0200

    fra timer

--- f08-eksempler-skeleton/src/animatedballs/Constants.java
+++ f08-eksempler-skeleton/src/animatedballs/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
 	public static final int PANE_SIZE = 500;
 	public static final int BALL_DIAMETER = 30;
 	public static final int MAX_VX_VY = 20;  
-	public static final int SLEEP_TIME = 80;  
+	public static final int SLEEP_TIME = 5;
 }
