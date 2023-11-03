6a98c4a58442113b63b3b276f640544fa91dd4b1
Lars-Petter Helland <lph@hvl.no>
2023-11-03 10:59:51 +0100

    En liten renaming ...

--- f22c-kryptokjop-haandstub/src/main/java/no/hvl/lph/dat108/problem/KryptoCalkulator.java
+++ f22c-kryptokjop-haandstub/src/main/java/no/hvl/lph/dat108/problem/KryptoCalkulator.java
@@ -8,14 +8,11 @@ import no.hvl.lph.dat108.Fiat;
 import no.hvl.lph.dat108.Krypto;
 
 @Service
-public class KryptokalkulatorNei {
+public class KryptoCalkulator {
 	
 	public double kjope(double antall, Krypto krypto, Fiat fiat) {
-
-		PrisfinnerKucoinNei prisfinner = new PrisfinnerKucoinNei();
-		
 		try {
-			return antall * prisfinner.finnPris(
+			return antall * PrisFinderKucoin.finnPris(
 					fiat.name(), krypto.name())
 					.get(krypto.name()).doubleValue();
 			
--- f22c-kryptokjop-haandstub/src/main/java/no/hvl/lph/dat108/problem/PrisFinderKucoin.java
+++ f22c-kryptokjop-haandstub/src/main/java/no/hvl/lph/dat108/problem/PrisFinderKucoin.java
@@ -7,10 +7,9 @@ import java.util.Map;
 import com.kucoin.sdk.KucoinClientBuilder;
 import com.kucoin.sdk.KucoinRestClient;
 
-public class PrisfinnerKucoinNei {
-	
+public class PrisFinderKucoin {
 
-	public Map<String, BigDecimal> finnPris(String fiatCode, String kryptoCode) throws IOException {
+	public static Map<String, BigDecimal> finnPris(String fiatCode, String kryptoCode) throws IOException {
 		
 		KucoinRestClient kucoinRestklient = new KucoinClientBuilder()
 				.withBaseUrl("https://openapi-v2.kucoin.com").buildRestClient();
